4e23c57a7dac9a4b5f51f15a1d00aebf8e883c6a
jujutheriault <146761189+jujutheriault@users.noreply.github.com>
2024-12-08 20:52:25 -0500

    Javadoc

--- distrib/src/main/java/dirogue/example/DIROgueClient.java
+++ distrib/src/main/java/dirogue/example/DIROgueClient.java
@@ -10,6 +10,10 @@ import java.util.Scanner;
  * sauvegarder des fichiers ou quitter l'application.
  */
 public class DIROgueClient {
+	/**
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		String serverAddress = null;
 		int serverPort = 0;
